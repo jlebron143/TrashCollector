db5f150fb1d015278f245daecf9b2ed41c208eac
jlebron143 <jlebron143@gmail.com>
2018-02-08 13:44:06 -0600

    Added Using DataAnnotations in customers

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -1,12 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
-using System.Globalization;
-using.System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -16,16 +9,11 @@ namespace TrashCollector.Models
     public class Customer
     {
         [Key]
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string ZipCode { get; set; }
+       
         public string userNameID { get; set; }
         public string pickUpDay { get; set; }
 
-        public virtual Schedule schedule { get; set; }
+      
   
     }
 }
